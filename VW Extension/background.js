ce862bda81cd05cf9b7c8c844b43359810ada499
Dannyjak <141071271+Dannyjak@users.noreply.github.com>
2024-07-26 21:24:32 -0400

    Update background.js

--- VW Extension/background.js
+++ VW Extension/background.js
@@ -18,3 +18,27 @@ chrome.action.onClicked.addListener((tab) => {
     );
 });
   
+
+// chrome.runtime.onInstalled.addListener(() => {
+//     console.log('Extension installed');
+// });
+
+// chrome.runtime.onMessage.addListener((message, sender, sendResponse) => {
+//     if (message.action === 'autofillData') {
+//         chrome.scripting.executeScript(
+//             {
+//                 target: { tabId: sender.tab.id },
+//                 function: autofillData,
+//                 args: [message.data]
+//             },
+//             () => {
+//                 if (chrome.runtime.lastError) {
+//                     console.error('Error:', chrome.runtime.lastError.message);
+//                 } else {
+//                     console.log('Content script executed successfully.');
+//                 }
+//             }
+//         );
+//     }
+// });
+  
